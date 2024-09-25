ceab3052a458bcba96423b5d1ffc58f7736eed4b
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-09-25 01:09:27 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write('Title')
+st.set_page_config(page_title="3D Basketball Shot Visualizer", page_icon='🏀',layout="wide")
+st.markdown(f'<h3 style="color: gray; text-align: center; font-size: 100px;">3D Basketball Shot Visualizer</h3>', unsafe_allow_html=True)
+st.subheader('')
+st.markdown('<h4 style="color: blue; text-align: center;">Welcome to the 3D Basketball Shot Visualizer! Explore games from College Basketball, NBA, and WNBA through 3D shot charts!</h4>', unsafe_allow_html=True)
+
