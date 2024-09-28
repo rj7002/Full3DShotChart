60aeed2a97f68871bad9a8f87134d8a188a13d1a
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-09-28 13:37:04 -0700

    Update basketballShot.py

--- basketballShot.py
+++ basketballShot.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 class BasketballShot:
-    def __init__(self, shot_start_x, shot_start_y, shot_id, play_description, shot_made, team,quarter,time,realquarter,typeshotchart,misspath,makepath):
+    def __init__(self, shot_start_x, shot_start_y, shot_id, play_description, shot_made, team,quarter,time,realquarter,typeshotchart,misspath,makepath,realquarter):
         self.hoop_loc_x = 25
         self.hoop_loc_y = None
         self.hoop_loc_z = 10
@@ -25,6 +25,7 @@ class BasketballShot:
         self.typeshotchart = typeshotchart
         self.misspath = misspath
         self.makepath = makepath
+        self.realquarter = realquarter
 
     def __adjust_shot_and_hoop_coordinates(self):
         print(self.team)
